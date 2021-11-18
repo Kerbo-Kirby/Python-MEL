957290b17e09e01676025edce777595aa9d4c3b5
peyton kerbo <10740520@my.uvu.edu>
2021-11-17 21:24:09 -0700

    covid free maybe

--- color.py
+++ color.py
@@ -2,16 +2,20 @@ import maya.cmds as cmds
 
 sels = cmds.ls(sl=True)
 
-
+cmds.group( 'nurbsCircle1' ,'nurbsCircle2',' nurbsCircle3')
+cmds.rename("group1","root")
 
 
 for sel in sels:
     
-    shapes = cmds.listRelatives(sel, children=True,shapes=True)
+    #shapes = cmds.listRelatives(sel, children=True,shapes=True)
     
+    cmds.rename(sel,f" what ")
+    
+    cmds.setAttr("nurbsCircleShape1" + ".overrideEnabled", 1)
     cmds.setAttr("nurbsCircleShape1.overrideColor",13)
     
+    cmds.xform( p=True, roo='yzx' )
     
-    cmds.group( 'nurbsCircle1' ,'nurbsCircle2',' nurbsCircle3' )
-    cmds.rename("group1","root")
-    print(sel + " head " + ' ctrl_ '.join(shapes))
+    cmds.DeleteHistory
+ #;'fddfg`   print(sel + " head " + ' ctrl_ '.join(shapes))
