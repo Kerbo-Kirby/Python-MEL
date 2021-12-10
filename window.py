322335020ad85c86bed355460e94efd2db9cc0af
peyton kerbo <10740520@my.uvu.edu>
2021-12-09 19:20:23 -0700

    button color

--- window.py
+++ window.py
@@ -1,18 +1,18 @@
 import maya.cmds as cmds
 
 
-window = cmds.window( title=" my window ", widthHeight=(200, 55) )
+window = cmds.window( title=" my window ", widthHeight=(500,100))
 
 cmds.columnLayout( adjustableColumn=True )
 
 cmds.button( label='Polysphere button', command = 'cmds.polySphere()', bgc=(.2,.3,1))
+cmds.button( label='rename me daddy', bgc=(.4,.3,.9))
 
-cmds.button( label=' Close ', bgc=(1,.5,.9),command=('cmds.deleteUI(\"' + window + '\", window=True)'))
+cmds.button( label=' Close',  bgc=(1,.5,.9),command=('cmds.deleteUI(\"' + window + '\", window=True)'))
 
-cmds.text( label='Name',  )
+cmds.text( label='Name', )
 
-cmds.showWindow( window )
-
+cmds.showWindow(window)
 
 
 
